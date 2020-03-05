23b314495d18dbbd7d85516798104728ee95d1ae
actoure <acherif.toure@gmail.com>
2020-03-05 02:11:47 +0100

    service screen

--- u_compass/lib/screens/services_screen.dart
+++ u_compass/lib/screens/services_screen.dart
@@ -2,12 +2,13 @@ import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:u_compass/widgets/bottom_navigation_bar_menu.dart';
 import 'package:u_compass/widgets/drawer_menu.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class Post {
   final String title;
   final String description;
 
-  Post(this.title, this.description);
+  Post(this.title,  this.description);
 }
 
 class ServicesScreen extends StatelessWidget {
@@ -38,72 +39,124 @@ class ServicesScreen extends StatelessWidget {
             ),
           ),
           Container(
-            height: 70,
+            height: 50,
             child: ListView(
               // This next line does the trick.
               scrollDirection: Axis.horizontal,
               children: <Widget>[
-                Container(
-                  padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                  margin: EdgeInsets.fromLTRB(5, 15, 5, 15),
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.all(Radius.circular(40.0))),
-                  child: Center(
-                    child: Wrap(
-                      children: <Widget>[
-                        Icon(Icons.restaurant),
-                        Text("Restauration")
-                      ],
+                SizedBox(width: 10),
+                RaisedButton.icon(
+                    onPressed: () {},
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(10.0),
                     ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                  margin: EdgeInsets.fromLTRB(5, 15, 5, 15),
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.all(Radius.circular(40.0))),
-                  child: Center(
-                    child: Wrap(
-                      children: <Widget>[
-                        Icon(Icons.restaurant),
-                        Text("Restauration")
-                      ],
+                    icon: Icon(
+                      Icons.restaurant,
                     ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                  margin: EdgeInsets.fromLTRB(5, 15, 5, 15),
-                  decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      borderRadius: BorderRadius.all(Radius.circular(40.0))),
-                  child: Center(
-                    child: Wrap(
-                      children: <Widget>[
-                        Icon(Icons.restaurant),
-                        Text("Restauration")
-                      ],
+                    label: Text(
+                        "Restaurant"
                     ),
-                  ),
+                    color: Colors.blue
                 ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-                  margin: EdgeInsets.fromLTRB(5, 15, 5, 15),
-                  decoration: BoxDecoration(
-                      color: Colors.lightGreen,
-                      borderRadius: BorderRadius.all(Radius.circular(40.0))),
-                  child: Center(
-                    child: Wrap(
-                      children: <Widget>[
-                        Icon(Icons.restaurant),
-                        Text("Restauration")
-                      ],
-                    ),
+                SizedBox(width: 10),
+                RaisedButton.icon(
+                  onPressed: () {},
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0),
                   ),
+                  icon: Icon(
+                    Icons.restaurant,
+                    size: 18.0,
+                  ),
+                  label: Text("Restaurant"),
+                  color: Colors.blue,
                 ),
+                SizedBox(width: 10),
+                RaisedButton.icon(
+                  onPressed: () {},
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0),
+                  ),
+                  icon: Icon(
+                    Icons.restaurant,
+                    size: 18.0,
+                  ),
+                  label: Text("Restaurant"),
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 10),
+                RaisedButton.icon(
+                  onPressed: () {},
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0),
+                  ),
+                  icon: Icon(
+                    Icons.restaurant,
+                  ),
+                  label: Text("Restaurant"),
+                  color: Colors.blue,
+                ),
+                SizedBox(width: 10),
               ],
+            ),
+          ),
+          Container(
+            height: 500,
+            child: StaggeredGridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12.0,
+                mainAxisSpacing: 12.0,
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                staggeredTiles: [
+                  StaggeredTile.extent(2, 130.0),
+                  /*StaggeredTile.extent(1, 150.0),
+                  StaggeredTile.extent(1, 150.0),
+                  StaggeredTile.extent(1, 150.0),
+                  StaggeredTile.extent(1, 150.0),*/
+                ],
+                children: <Widget>[
+                  Material(
+                    color: Colors.white,
+                    elevation: 14.0,
+                    shadowColor: Colors.black,
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(
+                                    Icons.restaurant,
+                                    color: Colors.amber,
+                                    size: 30.0,
+                                  ),
+                                ),
+                                Center(
+                                  child: Padding(
+                                    // Service Title
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      "Service 1",
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        fontSize: 20.0
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
             ),
           )
         ],
